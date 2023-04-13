7a8a0a7bc5b7c8aaee51ec52f7c41b81e08f7d69
ademirson1g <ademirson15@hotmail.com>
2023-04-13 03:49:54 +0200

    Add dropdown when certain key is pressed , refactor javascript code

--- scripts/dropdown-heading.js
+++ scripts/dropdown-heading.js
@@ -1,17 +1,47 @@
-// Get the input element and the dropdown menu
-const input = document.querySelector('input[type="text"]');
-const dropdown = document.querySelector('.absolute');
+function initHeadingOne() {
+  const inputElement = document.querySelector('input[type="text"]');
+  const dropdownElement = document.querySelector('.absolute');
+  const headingOneElement = document.getElementById('heading-1');
 
-// Add event listener to the input element
-input.addEventListener('input', function() {
-  // Check if the user has typed "/1" at the start of the input
-  if (input.value.startsWith('/1')) {
-    // Show the dropdown menu
-    dropdown.classList.remove('invisible');
-    dropdown.classList.add('visible', 'opacity-100');
-  } else {
-    // Hide the dropdown menu
-    dropdown.classList.remove('visible', 'opacity-100');
-    dropdown.classList.add('invisible');
+  if (!inputElement || !dropdownElement || !headingOneElement) {
+    throw new Error('Elements not found.');
   }
-});
+
+  inputElement.focus();
+  inputElement.select();
+
+  inputElement.addEventListener('input', () => {
+    if (inputElement.value.startsWith('/1')) {
+      dropdownElement.classList.remove('invisible');
+      dropdownElement.classList.add('visible', 'opacity-100');
+    } else {
+      dropdownElement.classList.remove('visible', 'opacity-100');
+      dropdownElement.classList.add('invisible');
+    }
+  });
+
+  inputElement.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      const newInputElement = document.createElement('input');
+      newInputElement.type = 'text';
+      newInputElement.placeholder = 'Type / for blocks, @ for blocks or docs peopl';
+      inputElement.parentNode.insertBefore(newInputElement, inputElement.nextSibling);
+      newInputElement.classList.add('w-full','focus:outline-none', 'mt-2');
+      newInputElement.focus();
+    }
+  });
+
+  headingOneElement.addEventListener('click', () => {
+    inputElement.value = inputElement.value.replace('/1', '');
+    inputElement.placeholder = 'Heading 1';
+    inputElement.focus();
+    inputElement.select();
+    headingOneElement.classList.add('font-bold', 'text-2xl');
+    inputElement.classList.add('font-bold', 'text-2xl');
+    dropdownElement.classList.remove('visible', 'opacity-100');
+    dropdownElement.classList.add('invisible');
+  });
+}
+
+initHeadingOne();
